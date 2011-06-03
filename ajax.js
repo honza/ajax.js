cd81d5b1b68cb765299a99344ff25fae45d7e919
Honza Pokorny <me@honza.ca>
2011-06-03 16:41:18 -0300

    Fix indentation.

--- ajax.js
+++ ajax.js
@@ -1,3 +1,25 @@
+/*
+ * ajax.js - simple ajax utility
+ *
+ * Usage:
+ *
+ *  ajax({
+ *    type: 'get',
+ *    url: 'http://example.com',
+ *    data: {name: 'John Smith'},
+ *    timeout: 5000,
+ *    onSuccess: function(data) {
+ *      console.log('Here is the data: ' + data);
+ *    },
+ *    onComplete: function() {
+ *
+ *    },
+ *    onError: function() {
+ *
+ *    }
+ *  });
+ */
+
 (function() {
 
   var ajax, serialize;
@@ -59,26 +81,26 @@
       xml.send(null);
     }
 
-        function httpSuccess(r) {
-          try { 
-            return !r.status && location.protocol == "file:" ||
-            ( r.status >= 200 && r.status < 300 ) ||
-            r.status == 304 ||
-            navigator.userAgent.indexOf("Safari") >= 0 && typeof r.status == "undefined"; 
-          } catch(e){}
-          return false;
-        }
+    function httpSuccess(r) {
+      try { 
+        return !r.status && location.protocol == "file:" ||
+        ( r.status >= 200 && r.status < 300 ) ||
+        r.status == 304 ||
+        navigator.userAgent.indexOf("Safari") >= 0 && typeof r.status == "undefined"; 
+      } catch(e){}
+      return false;
+    }
 
-        function httpData(r,type) {
-          var ct = r.getResponseHeader("content-type");
-          var data = !type && ct && ct.indexOf("xml") >= 0;
-          data = type == "xml" || data ? r.responseXML : r.responseText;
+    function httpData(r,type) {
+      var ct = r.getResponseHeader("content-type");
+      var data = !type && ct && ct.indexOf("xml") >= 0;
+      data = type == "xml" || data ? r.responseXML : r.responseText;
       if ( type == "script" )
         eval.call( window, data );
 
       return data;
 
-      }
+    }
   };
 
   this.ajax = ajax;
