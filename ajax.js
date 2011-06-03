f9ee9bded9c63aa30eeee7b723dd634586316d9b
Honza Pokorny <me@honza.ca>
2011-06-03 16:26:25 -0300

    Init commit.

---
+++ ajax.js
@@ -0,0 +1,86 @@
+(function() {
+
+  var ajax, serialize;
+
+  serialize = function(a) {
+
+    var s = [];
+
+    if (a.constructor == Array) {
+      for (var i=0; i < a.length; i++)
+        s.push(a[i].name + '=' + encodeURIComponent(a[i].value)); 
+    } else {
+      for (var j in a)
+        s.push(j + '=' + encodeURIComponent(a[j]));
+    }
+
+    return s.join('&');
+  };
+
+  ajax = function(options) {
+
+    options = {
+      type: options.type || "POST",
+      url: options.url || "",
+      timeout: options.timeout || 5000,
+      onSuccess: options.onSuccess || function(){},
+      onComplete: options.onComplete || function(){},
+      onError: options.onError || function(){},
+      data: options.data || null
+    };
+
+    var xml = new XMLHttpRequest();
+    xml.open(options.type, options.url, true);
+
+    var timeoutLength = options.timeout;
+    var requestDone = false;
+
+    setTimeout(function(){
+      requestDone = true;
+    }, timeoutLength);
+
+    xml.onreadystatechange = function(){
+      if ( xml.readyState == 4 && !requestDone ) {
+
+        if ( httpSuccess( xml ) ) {
+          options.onSuccess( httpData( xml, options.type ) );
+        } else {
+          options.onError();
+        }
+
+        options.onComplete();
+        xml = null;
+      }
+    };
+
+    if (options.type === 'POST') {
+      xml.send(serialize(options.data));
+    } else {
+      xml.send(null);
+    }
+
+        function httpSuccess(r) {
+          try { 
+            return !r.status && location.protocol == "file:" ||
+            ( r.status >= 200 && r.status < 300 ) ||
+            r.status == 304 ||
+            navigator.userAgent.indexOf("Safari") >= 0 && typeof r.status == "undefined"; 
+          } catch(e){}
+          return false;
+        }
+
+        function httpData(r,type) {
+          var ct = r.getResponseHeader("content-type");
+          var data = !type && ct && ct.indexOf("xml") >= 0;
+          data = type == "xml" || data ? r.responseXML : r.responseText;
+      if ( type == "script" )
+        eval.call( window, data );
+
+      return data;
+
+      }
+  };
+
+  this.ajax = ajax;
+
+})();
